49ac204e9d275704e94367d370f3a45a4d53b7cf
StephenTian11 <47480232+StephenTian11@users.noreply.github.com>
2019-03-04 12:01:40 -0500

    initial commit recipe finder backend.

---
+++ src/main/java/com/senior_project/recipe_finder/RecipeFinderApplication.java
@@ -0,0 +1,14 @@
+package com.senior_project.recipe_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeFinderApplication.class, args);
+    }
+
+}
+
